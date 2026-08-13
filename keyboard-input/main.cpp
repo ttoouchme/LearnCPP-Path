91344c675364f86d7b197675f5aefcd92113bb73
ttoouchme <pliatskomaksym@gmail.com>
2026-08-13 14:52:03 +0300

    keyboard input

---
+++ keyboard-input/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>  // for std::cout and std::cin
+
+int main()
+{
+    std::cout << "Enter three numbers: ";
+
+    int x{};
+    int y{};
+    int z{};
+    std::cin >> x >> y >> z;
+
+    std::cout << "You entered " << x <<", " << y << ", and " << z << ".\n";
+
+    return 0;
+}
