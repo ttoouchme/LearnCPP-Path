1fcf301d32d2903052fb41eb60e88f1fd88f4abb
ttoouchme <pliatskomaksym@gmail.com>
2026-08-13 18:17:44 +0300

    Add chapter 1 exercises and quiz programs

---
+++ game-zone-counter/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "=== GAME ZONE COUNTER ===" "\n";
+
+	std::cout << "Enter number of visitors: " "\n";						// кількість відвідувачів
+		int visitor { };
+			std::cin >> visitor;
+
+	std::cout << "Enter ticket price (UAH): " "\n";						// ціна квитку
+		int price { };
+			std::cin >> price;
+
+	std::cout << "Enter electricity costs (UAH): " "\n";			// ціна електрики
+		int elect { };
+			std::cin >> elect;
+
+	std::cout << "--- DAILY REPORT ---" "\n";
+	
+	int dirtcash { visitor * price};
+	std::cout << "Total income: " << dirtcash << " UAH " "\n";
+
+	int clearcash { dirtcash - elect};
+	std::cout << "Net profit: " << clearcash << " UAH " "\n";
+
+
+	return 0;
+}
